b68d4d261ec276a6b6c1bc4e6555fbe213cc5553
kostili-tec <weh4teyou@gmail.com>
2023-09-15 00:18:08 +0300

    chore: config vite for project

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import autoprefixer from 'autoprefixer';
+
+export const pathResolver = (p: string) => resolve(__dirname, '.', p);
+
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [autoprefixer],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': pathResolver('./src'),
+    },
+  },
+});
